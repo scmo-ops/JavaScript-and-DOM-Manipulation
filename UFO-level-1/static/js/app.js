e94e5be40f88324c60ea4f1a8377320d34d8f55f
scmo-ops <a01610125@exatec.tec.mx>
2021-06-26 10:10:11 -0500

    adjustments

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -25,27 +25,20 @@ button.on("click", function() {
 
     tbody.html("");
 
-    // Select the input date get the raw HTML nodes
     var inputElement = d3.select("#datetime");
-    // Get the value property of the input date, state, shape
     var inputValue = inputElement.property("value");
-    // console.log input value
     console.log(inputValue);
-    // Filter Data with datetime equal to input value
     var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
-    // console.log filter values
     console.log(filteredData);
-
 
     filteredData.forEach(function(selections) {
 
     console.log(selections);
-    // Append one table row `tr` for each UFO Sighting object
+
+    // I wanted to Append one table row for each sighting
     var row = tbody.append("tr");
-    // Use `Object.entries` to console.log each UFO Sighting value
     Object.entries(selections).forEach(function([key, value]) {
         console.log(key, value);
-        // Append a cell to the row for each value
         var cell = row.append("td");
         cell.text(value);
     });
